ccf13e4988d684d2f3962f1e9b51db505f05c589
Sangchi <prashnatchavan726386@gmail.com>
2024-08-03 12:10:17 +0530

    [prashant chavan]:calculate wage for 100 hr and 20 working days

--- emp.py
+++ emp.py
@@ -43,6 +43,38 @@ def check_monthly_wage():
     print(f"employee absent count={absent_count}")
     print(f"total_monthly_wage is={total_monthly_wage}")
 
+def check_for_hundred_hr_twenty_days():
+    daily_wage_count = 0
+    half_daily_count = 0
+    absent_count=0
+    total_hr=0
+
+    for i in range(20):
+        if(total_hr<=100):
+            choice = check_attend()
+            if choice == 1:
+                daily_wage_count += 1
+                total_hr +=8
+
+            elif choice == 2:
+                half_daily_count += 1
+                total_hr +=4
+
+            else:
+                absent_count +=1
+
+
+    daily_wage = 160
+    half_daily_wage = 80
+    total_monthly_wage = daily_wage * daily_wage_count + half_daily_wage * half_daily_count
+    print()
+    print("the employee wage for 100 hr and 20 working days given bellow:")
+    print(f"employee is part time prsent count={half_daily_count}")
+    print(f"emplyee is present full time count={daily_wage_count}")
+    print(f"employee absent count={absent_count}")
+    print(f"the total hr are {total_hr}")
+    print(f"total_monthly_wage is={total_monthly_wage}")
+
 def main():
 
     daily_hr = 8
@@ -63,9 +95,11 @@ def main():
             result = check_daily_half_wage(partime_hr, wage_per_hr)
             return result
 
+
         case 3:
             result=check_monthly_wage()
-            return result
+            result2=check_for_hundred_hr_twenty_days()
+            return result ,result2
         case _:
             result = "Choice is invalid"
 
@@ -74,3 +108,9 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+

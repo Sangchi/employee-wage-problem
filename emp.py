42a515c9c0b0ef93080e4737a747983711c73318
Sangchi <prashnatchavan726386@gmail.com>
2024-08-03 11:54:12 +0530

    [prashant chavan]:solved using Match cases

--- emp.py
+++ emp.py
@@ -5,7 +5,6 @@ def check_attend():
     choice = random.randint(0, 2)
     return choice
 
-
 def check_daily_wage(daily_hr, wage_per_hr):
 
     daily_wage = daily_hr * wage_per_hr
@@ -19,22 +18,30 @@ def check_daily_half_wage(partime_hr, wage_per_hr):
     print(f"Employee is present half time.") 
     print(f" half Daily wage is: {daily_half_wage}")
 
-
 def main():
 
     daily_hr = 8
+    partime_hr = 4
     wage_per_hr = 20
-    partime_hr=4
     choice=check_attend()
-    if(choice==1):
-        check_daily_wage(daily_hr, wage_per_hr)
 
-    elif(choice==2):
-        check_daily_half_wage(partime_hr,wage_per_hr)
-    else:
-        print("employee is absent")
+    match choice:
+        case 0:
+            result = 'Employee is absent'
+
+        case 1:
+            result = check_daily_wage(daily_hr, wage_per_hr)
+            return result
+
+        case 2:
+            result = check_daily_half_wage(partime_hr, wage_per_hr)
+            return result
+        case _:
+            result = "Choice is invalid"
+
+    print(result)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
 

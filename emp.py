be66ae31fa9d39d005d66b838e7d97b8843ce94f
Sangchi <prashnatchavan726386@gmail.com>
2024-08-03 11:46:43 +0530

    [prashant chavan:add parttime emplyee wage]

--- emp.py
+++ emp.py
@@ -2,7 +2,7 @@ import random
 
 def check_attend():
 
-    choice = random.randint(0, 1)
+    choice = random.randint(0, 2)
     return choice
 
 
@@ -13,13 +13,24 @@ def check_daily_wage(daily_hr, wage_per_hr):
     print(f"The daily wage is: {daily_wage}")
 
 
+def check_daily_half_wage(partime_hr, wage_per_hr):
+
+    daily_half_wage = partime_hr * wage_per_hr
+    print(f"Employee is present half time.") 
+    print(f" half Daily wage is: {daily_half_wage}")
+
+
 def main():
 
     daily_hr = 8
     wage_per_hr = 20
+    partime_hr=4
     choice=check_attend()
     if(choice==1):
         check_daily_wage(daily_hr, wage_per_hr)
+
+    elif(choice==2):
+        check_daily_half_wage(partime_hr,wage_per_hr)
     else:
         print("employee is absent")
 

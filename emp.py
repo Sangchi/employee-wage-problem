71212e036cba4b4063b37c83bfd29475cfc0f63e
Sangchi <prashnatchavan726386@gmail.com>
2024-08-03 11:38:38 +0530

    [prashant chavan]:calculate daily employee wage

--- emp.py
+++ emp.py
@@ -5,12 +5,21 @@ def check_attend():
     choice = random.randint(0, 1)
     return choice
 
+
+def check_daily_wage(daily_hr, wage_per_hr):
+
+    daily_wage = daily_hr * wage_per_hr
+    print(f"Employee is present full time.")
+    print(f"The daily wage is: {daily_wage}")
+
+
 def main():
 
+    daily_hr = 8
+    wage_per_hr = 20
     choice=check_attend()
     if(choice==1):
-        print("employee is present ")
-
+        check_daily_wage(daily_hr, wage_per_hr)
     else:
         print("employee is absent")
 
